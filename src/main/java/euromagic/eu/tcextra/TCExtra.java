5b22f9a5d916e7d13a87d30da2e93dd35871459f
tomster09090 <67745323+tomster09090@users.noreply.github.com>
2022-02-15 20:47:40 +0000

    INITIAL COMMIT

---
+++ src/main/java/euromagic/eu/tcextra/TCExtra.java
@@ -0,0 +1,17 @@
+package euromagic.eu.tcextra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TCExtra extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
