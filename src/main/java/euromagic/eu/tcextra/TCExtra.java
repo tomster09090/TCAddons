c3dce9eb8c62ecb1de1c965031e2a9a93e708507
tomster09090 <67745323+tomster09090@users.noreply.github.com>
2022-02-15 21:09:38 +0000

    Added SignActionMoney.java and added logic to register/unregister the SignAction

--- src/main/java/euromagic/eu/tcextra/TCExtra.java
+++ src/main/java/euromagic/eu/tcextra/TCExtra.java
@@ -1,17 +1,23 @@
 package euromagic.eu.tcextra;
 
+import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import euromagic.eu.tcextra.signactions.SignActionMoney;
+import org.bukkit.block.Sign;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TCExtra extends JavaPlugin {
 
+    public final SignActionMoney signActionMoney = new SignActionMoney();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        SignAction.register(signActionMoney);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        SignAction.unregister(signActionMoney);
     }
 }
---
+++ src/main/java/euromagic/eu/tcextra/signactions/SignActionMoney.java
@@ -0,0 +1,69 @@
+package euromagic.eu.tcextra.signactions;
+
+import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import com.bergerkiller.bukkit.tc.controller.MinecartMember;
+import com.bergerkiller.bukkit.tc.events.SignActionEvent;
+import com.bergerkiller.bukkit.tc.events.SignChangeActionEvent;
+import com.bergerkiller.bukkit.tc.signactions.SignAction;
+import com.bergerkiller.bukkit.tc.signactions.SignActionType;
+import com.bergerkiller.bukkit.tc.utils.SignBuildOptions;
+
+/**
+ * Displays a welcome message to the players in the train
+ */
+public class SignActionMoney extends SignAction {
+
+    @Override
+    public boolean match(SignActionEvent info) {
+        // Checks that the second line starts with 'welcome' (case-insensitive)
+        return info.isType("money");
+    }
+
+    @Override
+    public void execute(SignActionEvent info) {
+        // When a [train] sign is placed, activate when powered by redstone when the train
+        // goes over the sign, or when redstone is activated.
+        if (info.isTrainSign()
+                && info.isAction(SignActionType.GROUP_ENTER, SignActionType.REDSTONE_ON)
+                && info.isPowered() && info.hasGroup()
+        ) {
+            for (MinecartMember<?> member : info.getGroup()){
+                sendGreetingForCart(info, member);
+            }
+            return;
+        }
+
+        // When a [cart] sign is placed, activate when powered by redstone when each cart
+        // goes over the sign, or when redstone is activated.
+        if (info.isCartSign()
+                && info.isAction(SignActionType.MEMBER_ENTER, SignActionType.REDSTONE_ON)
+                && info.isPowered() && info.hasMember()
+        ) {
+            sendGreetingForCart(info, info.getMember());
+            return;
+        }
+    }
+
+    public void sendGreetingForCart(SignActionEvent info, MinecartMember<?> member) {
+        // Lines 3 and 4 configure the message to send
+        String message = info.getLine(2) + info.getLine(3);
+
+        // Send to all player passengers of the cart (could be multiple seats!)
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "eco give " + member.getLocalizedName() + " " + info.getLine(2));
+    }
+
+    @Override
+    public boolean build(SignChangeActionEvent event) {
+        // This is executed when a player places down a sign matching this sign action
+        // Permissions are checked and then a message is displayed to the player
+        // For simplicity you can use the SignBuildOptions API for this.
+        // You are free to use your own code here that checks permissions/etc.
+        return SignBuildOptions.create()
+                .setName(event.isCartSign() ? "cart money distributor" : "train money distributor")
+                .setDescription("pays all players within the train/cart a specified amount")
+                .handle(event.getPlayer());
+    }
+}
